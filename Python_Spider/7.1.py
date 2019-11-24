a39cd3603333281568ad757df5a23de4da49934b
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-11-24 09:39:14 +0800

    start

--- Python_Spider/7.1.py
+++ Python_Spider/7.1.py
@@ -1,1 +1,21 @@
 '''new file, section 7.1'''
+import json
+
+
+person = \
+    {
+      'basic_info': {
+          'name': 'kingname',
+          'age': 24,
+          'sex': '男',
+          'merry': False
+      },
+      'work_info':{
+          'salary': 99999,
+          'position': 'engineer',
+          'department': None
+      }
+    }
+
+person_json = json.dumps(person, indent = 4)
+print(person_json)
