3ff9dfcf72a9e2772b8864acf66b94054e9beedb
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-11-27 22:28:43 +0800

    7.1.6  ended.

--- Python_Spider/7.1.py
+++ Python_Spider/7.1.py
@@ -34,24 +34,32 @@ import re
 # code_dict = json.loads(code_json)
 # print(code_dict['code'])
 
-# 7.1.5.1
+
+# 7.1.5
 # the html
-url = 'http://exercise.kingname.info/exercise_ajax_3.html'
-first_ajax_url = 'http://exercise.kingname.info/ajax_3_backend'
-secend_ajax_url = 'http://exercise.kingname.info/ajax_3_postbackend'
+# url = 'http://exercise.kingname.info/exercise_ajax_3.html'
+# first_ajax_url = 'http://exercise.kingname.info/ajax_3_backend'
+# secend_ajax_url = 'http://exercise.kingname.info/ajax_3_postbackend'
+#
+# page_html = requests.get(url).content.decode()
+# secret_2 = re.search("secret_2 = '(.*?)'", page_html).group(1)
+#
+# ajax_1_json = requests.get(first_ajax_url).content.decode()
+# ajax_1_dict = json.loads(ajax_1_json)
+# secret_1 = ajax_1_dict['code']
+#
+# ajax_2_json = requests.post(secend_ajax_url, json = {
+#     'name': "青南",
+#     'age': 24,
+#     'secret1': secret_1,
+#     'secret2': secret_2}).content.decode()
+# ajax_2_dict = json.loads(ajax_2_json)
+# code = ajax_2_dict['code']
+# print(f'最终页面显示的内容：{code}')
 
-page_html = requests.get(url).content.decode()
-secret_2 = re.search("secret_2 = '(.*?)'", page_html).group(1)
 
-ajax_1_json = requests.get(first_ajax_url).content.decode()
-ajax_1_dict = json.loads(ajax_1_json)
-secret_1 = ajax_1_dict['code']
-
-ajax_2_json = requests.post(secend_ajax_url, json = {
-    'name': "青南",
-    'age': 24,
-    'secret1': secret_1,
-    'secret2': secret_2}).content.decode()
-ajax_2_dict = json.loads(ajax_2_json)
-code = ajax_2_dict['code']
-print(f'最终页面显示的内容：{code}')
+# 7.1.6
+url = 'http://exercise.kingname.info/ajax_4_backend'
+code_json = requests.post(url, json={'username': 'kingname', 'password': 'genius'}).content.decode()
+code_dict = json.loads(code_json)
+print(code_dict['code'])
