1ae49aa48d553c253aca005fbf4d2c5231a0dc01
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-12-01 17:20:38 +0800

    7.4  ended.

--- Python_Spider/7.4_乐视_大鱼海棠评论爬取.py
+++ Python_Spider/7.4_乐视_大鱼海棠评论爬取.py
@@ -4,14 +4,19 @@ import json
 
 class LetvSpider(object):
 
-    COMMENT_URL = 'http://api.my.le.com/vcm/api/list?jsonp=jQuery19106596654532269319_1575164258089&type=video&rows=20&page=1&sort=&cid=1&source=1&xid={xid}&pid={pid}&ctype=cmt%2Cimg%2Cvote&listType=1&_=1575164258095'
+    COMMENT_URL = 'http://api.my.le.com/vcm/api/list?jsonp=jQuery19106596654532269319_1575164258089&type=video' \
+                  '&rows=20&page=1&sort=&cid=1&source=1&xid={xid}&pid={pid}&ctype=cmt%2Cimg%2Cvote' \
+                  '&listType=1&_=1575164258095'
 
     HEADERS = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN, zh; q=0.9',
         'Connection': 'keep-alive',
-        'Cookie': 'tj_lc = 32696bb81587f2e7258c350f8518994e; tj_uuid = -_15751637970179369968;tj_env = 1; ark_uuid = ck-d3092f27-687d-457f-b1c6-ba3ac90877c4-1201-092959;bd_xid = 32696bb81587f2e7258c350f8518994e; ssoCookieSynced = 1; language = zh-cn; sso_curr_country = CN; tj_v2c = -26680567_1',
+        'Cookie': 'tj_lc = 32696bb81587f2e7258c350f8518994e; tj_uuid = -_15751637970179369968;tj_env = 1; '
+                  'ark_uuid = ck-d3092f27-687d-457f-b1c6-ba3ac90877c4-1201-092959;'
+                  'bd_xid = 32696bb81587f2e7258c350f8518994e; ssoCookieSynced = 1; language = zh-cn; '
+                  'sso_curr_country = CN; tj_v2c = -26680567_1',
         'Host': 'api.my.le.com',
         'Referer': 'http://www.le.com/ptv/vplay/26680567.html',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
