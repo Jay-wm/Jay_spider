c5ae19037e1d010b6b37c87e72bb6c86671645b6
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-12-18 12:39:16 +0800

    Update 10.1.4.1_获取屏幕文字.py

--- Python_Spider/10.1.4.1_获取屏幕文字.py
+++ Python_Spider/10.1.4.1_获取屏幕文字.py
@@ -3,8 +3,6 @@ from uiautomator import Device
 app_list = []
 device = Device()
 
-
-
 for i in range(2):
     if i !=0:
         device(scrollable=True).scroll.vert.forward()
@@ -12,12 +10,13 @@ for i in range(2):
     app_download_ranking = device(index="0", className = "android.widget.Image")
 
     # print the app name of the current page
-    for app in app_download_ranking:
-        if app not in app_list:
-            app_list.append(app)
-            print(app.text)
-    #
-    # for a in app_list:
-    #     print(a.text)
+    for m in range(len(app_download_ranking)):
+        if app_download_ranking[m] in app_list:
+            
+        else:
+            app_list.append(app_download_ranking[m])
+            
+for a in app_list:
+    print(a.text)
 
 
