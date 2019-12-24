1e448818003cc96c8197ee8e2abd76c3effdb3e2
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-12-24 12:59:59 +0800

    update pipeline

--- Python_Spider/chapter_11/11.2.2_在scrapy中使用xpath/baidu/baidu/pipelines.py
+++ Python_Spider/chapter_11/11.2.2_在scrapy中使用xpath/baidu/baidu/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymongo
+from scrapy.conf import settings
 
 class BaiduPipeline(object):
+    def __init__(self):
+        host = settings['MONGODB_HOST']
+        port = settings['MONGODB_PORT']
+        db_name = settings['MONGODB_DBNAME']
+        client = pymongo.MongoClient(host = host, port = port)
+        db = client[db_name]
+        self.post = db(settings['MONGODB_DOCNAME'])
+        
     def process_item(self, item, spider):
+        person_info = dict(itme)
+        self.post.insert(person_info)
         return item
